5b9f85d9a1c5719e94ed0b23136016b28a638332
Naty11008 <utm24090757@utma.edu.mx>
2025-11-08 17:50:25 -0600

    WORK1

---
+++ Persona.cs
@@ -0,0 +1,33 @@
+﻿namespace backend
+{
+    public class Persona
+    {
+        public int Edad { get; set; }
+        public bool EsEstudiante { get; set; }
+
+    }
+    public class Costo
+    {
+        public static decimal CalcularCosto(Persona persona)
+        {
+            decimal cobro = 0;
+            if (persona.Edad < 16)
+            {
+                cobro = 0;
+            }
+            else if (persona.Edad > 60)
+            {
+                cobro = 0;
+            }
+            else if (persona.EsEstudiante)
+            {
+                cobro = 0;
+            }
+            else
+            {
+                cobro = 100;
+            }
+            return cobro;
+        }
+    }
+}
