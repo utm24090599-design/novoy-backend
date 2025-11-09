e85e910c10947465cd11f80417f3c998c70635e3
Cesar Eduardo Castañeda López <utm24090599@utma.edu.mx>
2025-11-08 21:36:23 -0600

    Fix(Program.cs): Agregado el deploy del frontend

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173") // origen del frontend
+        policy.WithOrigins("http://localhost:5173", "https://novoy-frontend-j6pyulsgg-utm24090599-designs-projects.vercel.app") // origen del frontend
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
